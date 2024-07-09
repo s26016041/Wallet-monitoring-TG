16d7c9f7269385bc213e356b4f7f127950cc8ca1
s26016041@gmail.com <s26016041@gmail.com>
2024-07-09 22:00:41 +0800

    Error handling

--- src/chatStates/chatStates.js
+++ src/chatStates/chatStates.js
@@ -12,9 +12,9 @@ class ChatStates {
   }
 
   /**
-   * 
-   * @param {number} num 
-   * @param {number} decimals 
+   *
+   * @param {number} num
+   * @param {number} decimals
    * @returns {number}
    */
   formatNumber(num, decimals) {
@@ -24,131 +24,155 @@ class ChatStates {
       return num.toFixed(decimals); // 如果是小数，返回指定位数的小数
     }
   }
+  // SOL 價格
   async getSolPrice() {
     let response = await axios.get(
       "https://api.coingecko.com/api/v3/simple/price?ids=solana&vs_currencies=usd"
     );
-    this.sol_usd = response.data.solana.usd
+    this.sol_usd = response.data.solana.usd;
   }
   start() {
     this.solPriceUsd = setInterval(async () => {
       this.getSolPrice();
     }, 600000);
     this.printFiveInterval = setInterval(async () => {
-      // SOL 價格
+      try {
+        for (let [chat_id, data] of this.states) {
+          let state_obj = this.states.get(chat_id);
+          // console.log(this.states);
+          // console.log(this.sol_usd);
+          if (data && data.sol_wallet_map && data.sol_wallet_map.size > 0) {
+            for (let [address, name] of data.sol_wallet_map.entries()) {
+              let SignatureArray = await this.sol_wallet.getSignatureArray(
+                address
+              );
+              if (SignatureArray.length == 0) {
+                continue;
+              }
+              // console.log(address)
+              // 沒有最後一筆哈希的話幫她添加
+              if (!data.address_signature.has(address)) {
+                if (state_obj) {
+                  // 修改状态对象中的 address_signature 改為最新簽名
+                  state_obj.address_signature.set(
+                    address,
+                    SignatureArray[0].signature
+                  );
+                  // console.log(SignatureArray)
+                  // 将更新后的状态对象重新存回 Map 中
+                  this.states.set(chat_id, state_obj);
+                }
+                continue;
+              }
+              // 把新的哈希交易都通知
+              for (let signature of SignatureArray) {
+                if (
+                  signature.signature ==
+                  state_obj.address_signature.get(address)
+                ) {
+                  break;
+                }
+                let transaction = await this.sol_wallet.getTransaction(
+                  signature.signature
+                );
+                // 一些特殊交易室會回傳未定義
+                if (
+                  !transaction ||
+                  !transaction.meta.postTokenBalances ||
+                  transaction.meta.postTokenBalances.length === 0 ||
+                  !transaction.meta.preTokenBalances ||
+                  transaction.meta.preTokenBalances.length === 0
+                ) {
+                  continue;
+                }
 
-      for (let [chat_id, data] of this.states) {
-        let state_obj = this.states.get(chat_id);
-        // console.log(this.states);
-        // console.log(this.sol_usd);
-        if (data && data.sol_wallet_map && data.sol_wallet_map.size > 0) {
-          for (let [address, name] of data.sol_wallet_map.entries()) {
-            let SignatureArray = await this.sol_wallet.getSignatureArray(
-              address
-            );
-            if (SignatureArray.length ==0){continue}
-            // console.log(address)
-            // 沒有最後一筆哈希的話幫她添加
-            if (!data.address_signature.has(address)) {
+                if (transaction.meta.preTokenBalances) {
+                  let solscan_wallet_url = `https://solscan.io/account/${address}`;
+                  let telegram_message = ``;
+                  let post_sol = transaction.meta.postBalances;
+                  let post_token = transaction.meta.postTokenBalances;
+                  let pre_sol = transaction.meta.preBalances;
+                  let pre_token = transaction.meta.preTokenBalances;
+                  let token_address,
+                    token_name,
+                    accountIndex,
+                    lamports,
+                    photon_url,
+                    solscan_token_url,
+                    price,
+                    signature_url;
+                  let token_number = 0;
+                  let sol_number = 0;
+
+                  telegram_message += `[<a href="${solscan_wallet_url}">${name}</a>]\n`;
+                  for (let pre_token_data of pre_token) {
+                    if (pre_token_data.owner === address) {
+                      token_address = pre_token_data.mint;
+                      if (!pre_token_data.uiTokenAmount.uiAmount == null) {
+                        token_number += pre_token_data.uiTokenAmount.uiAmount;
+                      }
+                      accountIndex = pre_token_data.accountIndex - 1;
+                    }
+                  }
+                  for (let post_token_data of post_token) {
+                    if (post_token_data.owner === address) {
+                      token_address = post_token_data.mint;
+                      if (!post_token_data.uiTokenAmount.uiAmount == null) {
+                        token_number -= post_token_data.uiTokenAmount.uiAmount;
+                      }
+                      accountIndex = post_token_data.accountIndex - 1;
+                    }
+                  }
+                  solscan_token_url = `https://solscan.io/token/${token_address}`;
+                  token_name = await this.sol_wallet.getTokenName(
+                    token_address
+                  );
+                  lamports = pre_sol[accountIndex] - post_sol[accountIndex];
+                  sol_number = this.sol_wallet.lamportToSol(Math.abs(lamports));
+                  price =
+                    (sol_number * this.solPriceUsd) / Math.abs(token_number);
+                  photon_url = `https://photon-sol.tinyastro.io/zh/lp/${token_address}`;
+                  signature_url = `https://solscan.io/tx/${signature}`;
+                  if (token_number < 0) {
+                    telegram_message += `<b>🔴 賣 ${sol_number} SOL</b>\n`;
+                    telegram_message += `<b>🟢 買 ${Math.abs(
+                      token_number
+                    )} [<a href="${solscan_token_url}">${token_name}</a>]</b>\n`;
+                    telegram_message += `價格: ${price}\n`;
+                    telegram_message += `<a href="${photon_url}">photon</a>\n`;
+                    telegram_message += `代幣地址: <code>${token_address}</code>\n`;
+                    telegram_message += `<a href="${signature_url}">交易情況</a>\n`;
+                  } else {
+                    telegram_message += `<b>🔴 賣 ${Math.abs(
+                      token_number
+                    )} [<a href="${solscan_token_url}">${token_name}</a>]</b>\n`;
+                    telegram_message += `<b>🟢 買 ${sol_number} SOL</b>\n`;
+                    telegram_message += `價格: ${price}\n`;
+                    telegram_message += `<a href="${photon_url}">photon</a>\n`;
+                    telegram_message += `代幣地址: <code>${token_address}</code>\n`;
+                    telegram_message += `<a href="${signature_url}">交易情況</a>\n`;
+                  }
+
+                  this.bot.sendMessage(chat_id, telegram_message, {
+                    parse_mode: "HTML",
+                  });
+                }
+              }
+              // 換成最新的哈希
               if (state_obj) {
                 // 修改状态对象中的 address_signature 改為最新簽名
                 state_obj.address_signature.set(
                   address,
                   SignatureArray[0].signature
                 );
-                // console.log(SignatureArray)
                 // 将更新后的状态对象重新存回 Map 中
                 this.states.set(chat_id, state_obj);
               }
-              continue;
-            }
-            // 把新的哈希交易都通知
-            for (let signature of SignatureArray) {
-              if (signature.signature == state_obj.address_signature.get(address)) {
-                break;
-              }
-              let transaction = await this.sol_wallet.getTransaction(signature.signature);
-              // 一些特殊交易室會回傳未定義
-              if (
-                !transaction || !transaction.meta.postTokenBalances || transaction.meta.postTokenBalances.length === 0 ||
-                !transaction.meta.preTokenBalances || transaction.meta.preTokenBalances.length === 0
-              ) {
-                continue;
-              }
-
-              if (transaction.meta.preTokenBalances) {
-                let solscan_wallet_url = `https://solscan.io/account/${address}`;
-                let telegram_message = ``;
-                let post_sol = transaction.meta.postBalances;
-                let post_token = transaction.meta.postTokenBalances;
-                let pre_sol = transaction.meta.preBalances;
-                let pre_token = transaction.meta.preTokenBalances;
-                let token_address, token_name, accountIndex, lamports, photon_url, solscan_token_url, price,signature_url
-                let token_number = 0
-                let sol_number = 0
-
-
-                telegram_message += `[<a href="${solscan_wallet_url}">${name}</a>]\n`
-                for (let pre_token_data of pre_token) {
-                  if (pre_token_data.owner === address) {
-                    token_address = pre_token_data.mint
-                    if (!pre_token_data.uiTokenAmount.uiAmount == null) {
-                      token_number += pre_token_data.uiTokenAmount.uiAmount
-                    }
-                    accountIndex = pre_token_data.accountIndex - 1
-                  }
-                }
-                for (let post_token_data of post_token) {
-                  if (post_token_data.owner === address) {
-                    token_address = post_token_data.mint
-                    if (!post_token_data.uiTokenAmount.uiAmount == null) {
-                      token_number -= post_token_data.uiTokenAmount.uiAmount
-                    }
-                    accountIndex = post_token_data.accountIndex - 1
-                  }
-                }
-                solscan_token_url = `https://solscan.io/token/${token_address}`
-                token_name = await this.sol_wallet.getTokenName(token_address);
-                lamports = pre_sol[accountIndex] - post_sol[accountIndex]
-                sol_number = this.sol_wallet.lamportToSol(Math.abs(lamports))
-                price = sol_number / Math.abs(token_number)
-                photon_url = `https://photon-sol.tinyastro.io/zh/lp/${token_address}`
-                signature_url =`https://solscan.io/tx/${signature}`
-                if (token_number < 0) {
-                  telegram_message += `<b>🔴 賣 ${sol_number} SOL</b>\n`;
-                  telegram_message += `<b>🟢 買 ${Math.abs(token_number)} [<a href="${solscan_token_url}">${token_name}</a>]</b>\n`;
-                  telegram_message += `價格: ${price}\n`;
-                  telegram_message += `<a href="${photon_url}">photon</a>\n`;
-                  telegram_message += `代幣地址: <code>${token_address}</code>\n`;
-                  telegram_message += `<a href="${signature_url}">交易情況</a>\n`;
-                } else {
-                  telegram_message += `<b>🔴 賣 ${Math.abs(token_number)} [<a href="${solscan_token_url}">${token_name}</a>]</b>\n`;
-                  telegram_message += `<b>🟢 買 ${sol_number} SOL</b>\n`;
-                  telegram_message += `價格: ${price}\n`;
-                  telegram_message += `<a href="${photon_url}">photon</a>\n`;
-                  telegram_message += `代幣地址: <code>${token_address}</code>\n`;
-                  telegram_message += `<a href="${signature_url}">交易情況</a>\n`;
-                }
-
-                this.bot.sendMessage(chat_id, telegram_message, {
-                  parse_mode: "HTML",
-                });
-
-
-              }
-            }
-            // 換成最新的哈希
-            if (state_obj) {
-              // 修改状态对象中的 address_signature 改為最新簽名
-              state_obj.address_signature.set(
-                address,
-                SignatureArray[0].signature
-              );
-              // 将更新后的状态对象重新存回 Map 中
-              this.states.set(chat_id, state_obj);
             }
           }
         }
+      } catch (error) {
+        console.log("循環發生錯誤: ", error);
       }
     }, 5000);
   }
@@ -178,7 +202,6 @@ class ChatStates {
   }
   // 每次接收到的訊息會看該群組是否有正在使用指令
   setSolWallet(chat_id, message) {
-
     if (this.states.get(chat_id).add_wallet) {
       let wallet_address, name;
       let parts = message.split(" ");
